5986989fcc25461f665855407d00a8cf7687f52e
aoyama <aoyama@val.co.jp>
2020-12-22 15:07:25 +0900

    add npm package

--- handlers/bye.js
+++ handlers/bye.js
@@ -1,9 +1,12 @@
 "use strict";
 
+const uuid = require("uuid");
+
 exports.handler = async (event) => {
     console.log("event:", JSON.stringify(event, null, 2));
     let result = {
         message: "Goodbye world! now = " + new Date().toString(),
+        uuid: uuid.v4(),
     };
     return {
         statusCode: 200,
