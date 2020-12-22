f2a65186ba19c0f16d9291c2b1854b0ac4ff8ebf
aoyama <aoyama@val.co.jp>
2020-12-22 13:13:49 +0900

    init

---
+++ bye/index.js
@@ -0,0 +1,10 @@
+exports.handler = async (event) => {
+    console.log("event:", JSON.stringify(event, null, 2));
+    let result = {
+        message: "Goodbye world! now = " + new Date().toString(),
+    };
+    return {
+        statusCode: 200,
+        body: JSON.stringify(result, null, 2),
+    };
+};
